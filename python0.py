9a1237b20654475e85e24e48487be4c58250271f
Santo <santosh@transak.com>
2023-06-01 16:22:43 +0530

    Remove duplicate

---
+++ python0.py
@@ -0,0 +1,21 @@
+# Program to remove duplicates from a list
+arr = [2,2,2,2,2,2,2,2,2]
+new_arr = []
+for i in range(len(arr)):
+    if arr[i] not in new_arr:
+        new_arr.append(arr[i])  
+print(new_arr)
+
+        
+
+
+   
+        
+
+    
+        
+    
+        
+    
+    
+    
