4fc2373651370a056de0688d18066589cf169c72
Santo <santosh@transak.com>
2023-06-01 16:29:50 +0530

    Yolooo

--- pyth02.py
+++ pyth02.py
@@ -1,6 +1,9 @@
-arr = [2,2,2,2,2,2,2,2,2]
+arr = [1, 2, 2, 2]
 new_arr = []
-for i in range(len(arr)):
-    if arr[i] not in new_arr:
-        new_arr.append(arr[i])  
+
+for i in arr:
+    if i not in new_arr:
+        new_arr.append(i)
+
 print(new_arr)
+
