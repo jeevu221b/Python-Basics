495ea2623ab5d15db31d58e3d123fd4f822b714e
Santo <santosh@transak.com>
2023-06-06 16:46:46 +0530

    Reverse String

---
+++ Reverse_string.py
@@ -0,0 +1,19 @@
+name = "Madam"
+reversed = " "
+# x = len(name) - 1
+# while(x>=0):
+#     # print (x)
+#     reversed = reversed + name[x]
+#     x -= 1
+    
+# print(reversed)
+
+for i in range(len(name)-1, -1, -1):
+    print(i)
+    reversed = reversed + name[i]
+print(reversed)
+    
+
+    
+
+
