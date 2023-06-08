2ffb911419fd80f03b6cdfe6b468927601576b49
Santo <santosh@transak.com>
2023-06-08 14:10:31 +0530

    Find a missing element in an array

---
+++ missingNumber.py
@@ -0,0 +1,12 @@
+arr = [6,1,2,8,3,4,7,10,5]
+new_arr = []
+for i in range(1,len(arr)+2):
+    if i not  in arr:
+        new_arr.append(i)
+        
+
+print(new_arr)
+            
+                           
+            
+
