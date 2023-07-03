b83c654593a58e027316bc4f1bf3411d737f597a
Santo <santosh@transak.com>
2023-07-03 13:25:03 +0530

    Exercise on objects

--- MajorityElement.py
+++ MajorityElement.py
@@ -1,19 +1,19 @@
 def majorityElement(array, size):
     new_arr = []
-    length = size/2
+    majorityElement = {"element": " ", "repetion":-1}
+    length = size / 2
     print(length, "length")
     duplicateCount = 0
 
     for x in range(size):
-        for y in range(x+1, size):
+        for y in range(x + 1, size):
             if array[x] == array[y]:
                 new_arr.append(array[y])
     print(new_arr)
     # for z in range(len(new_arr)):
 
-
     # return (new_arr)
 
 
-output = majorityElement([2, 3, 5, 2, 4, 3], 6)
+output = majorityElement([2, 3,2], 2)
 # print(output)
---
+++ RepeatedElements.py
@@ -0,0 +1,14 @@
+users = {"logan": {"address": {"pinCode": 110019}}}
+users["logan"]["address"]["pinCode"] = 4848448
+users["john"] = {"address": {"pincode": 737393}}
+
+print(users)
+
+# while True:
+#     Name = input("Enter the name:")
+#     pin = input("Enter the pincode:")
+#     ask = input("Are there more names and pincodes?")
+#     users["name"] = [Name]
+#     if ask != "yes":
+#         break
+# print(users)
