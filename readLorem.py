724ea28b9723b7cc01c0597d9216aeb407ee3393
Santo <santosh@transak.com>
2023-08-22 14:32:34 +0530

    Users in ascending order

--- readLorem.py
+++ readLorem.py
@@ -6,7 +6,6 @@ str = ""
 arr = []
 new = {}
 for x in range(len(content)):
-
     # print(content[x], ":letter")
     if content[x] != " " and content[x] != "\n":
         str = str + content[x]
@@ -20,12 +19,25 @@ for x in range(len(content)):
 for key in words:
     new[key] = words[key]
     arr.append(new)
-    # print(new)
-    # print(arr)
     new = {}
 
-print(arr[1])
+key = []
+for x in range(len(arr)):
+    key = list(arr[x].keys())
+    xkey = key[0]
+    for y in range(len(arr)):
+        key = list(arr[y].keys())
+        ykey = key[0]
 
-# for a in range(len(arr)):
-#     for b in range(a+1, len(arr)):
-#         if arr
+        if arr[x][xkey] < arr[y][ykey]:
+            temp = arr[x][xkey]
+            arr[x][xkey] = arr[y][ykey]
+            arr[y][ykey] = temp
+
+kesi = []
+k = []
+for z in range(len(arr)):
+    kesi = list(arr[z].keys())
+    x = kesi[0]
+    k = list(arr[z].keys())
+    print(k[0], "-->", arr[z][x])
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,28 @@
-for key in words:
-#     for i in range(len)
+# obj1 = [
+#     {"Lorem": 26},
+#     {"ipsum": 26},
+#     {"dolor": 31},
+#     {"sit": 26},
+#     {"amet,": 15},
+#     {"consetetur": 11},
+#     {"sadipscing": 12},
+# ]
+# values = []
+# users = []
+# obj2 = []
+# for dictionary in obj1:
+#     # Loop through the key-value pairs in each dictionary
+#     for key, value in dictionary.items():
+#         # print(values)
+#         values.append(key)
+#         users.append(value)
+# # print(values[0])
+# # print(users[0])
+# for x in range(len(users)):
+num = ["je", "cf", "dd"]
+obj = {}
+obj = num[0]
+obj[num[0]] = "hey"
+print(obj)
+# for x in range(len(num)):
+#     obj[num] = x
