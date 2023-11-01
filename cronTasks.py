fa9f053ed2dfbdee25dc3b5c075c776819c60f74
jeevu221b <jeewanpany4@gmail.com>
2023-11-01 12:13:54 +0530

    Added the 3 seconds interval

--- cronTasks.py
+++ cronTasks.py
@@ -1,27 +1,28 @@
 import sqlite3
+# Import the correct scheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
-import notifier
-import time
-
-conn = sqlite3.connect("app.db")
-cursor = conn.cursor()
-
+import notifyy
+# conn = sqlite3.connect("app.db")
+# cursor = conn.cursor()
 scheduler = BackgroundScheduler()
 
 
-# @TO_DO: Take userId as a parameter
 def getDueTasks():
+
+    conn = sqlite3.connect("app.db")
+    cursor = conn.cursor()
+
     allTasks = []
     t = datetime.now()
     curr_time = t.strftime("%H:%M:%S")
-    cursor.execute("SELECT * FROM tasks")
-    #    where duedate > ?", (curr_time,))
+    cursor.execute("SELECT * FROM tasks WHERE duedate > ?", (curr_time,))
     tasks = cursor.fetchall()
     for ids in tasks:
         __task = {
-            "user_id": ids[0],
-            "task_id": ids[1],
+            "task_id": ids[0],
+            "user_id": ids[1],
             "taskname": ids[2],
             "duedate": ids[3],
         }
@@ -29,18 +30,48 @@ def getDueTasks():
     print(allTasks)
     return allTasks
 
+# getDueTasks()
+
+
+# def cronJob(hr, min, sec, taskname):
+#     scheduler.add_job(
+#         notifyy.notifier, "cron", hour=hr, minute=min, second=sec, args=[taskname]
+#     )
+
 
 def cronos():
-    scheduler.add_job(getDueTasks, "interval", seconds=3)
-    scheduler.start()
+    print("Inside cronos")
+    # Create a BlockingScheduler instance
+    cronScheduler = BlockingScheduler()
+
+    # Schedule the getDueTasks function to run every 3 seconds (adjust the interval as needed)
+    cronScheduler.add_job(getDueTasks, 'interval', seconds=3)
+
+    # Start the scheduler
+    print("Yoppie")
+    cronScheduler.start()
+    print("Yippie")
+
+    ids = getDueTasks()
+    print(f"Length = {len(ids)}")
+
+    for id in ids:
+        duedate = id["duedate"]
+        taskname = id["taskname"]
+        time_obj = datetime.strptime(duedate, "%H:%M:%S")
+        hour = time_obj.hour
+        minute = time_obj.minute
+        second = time_obj.second
+        # cronJob(hour, minute, second, taskname)
+        print("Hey!")
 
 
-while True:
+if __name__ == '__main__':
     cronos()
-    time.sleep(1)
 
 
 # ids = getDueTasks()
+# print(f"Length = {len(ids)}")
 
 # for id in ids:
 #     duedate = id["duedate"]
@@ -49,7 +80,6 @@ while True:
 #     hour = time_obj.hour
 #     minute = time_obj.minute
 #     second = time_obj.second
-#     print(taskname)
 #     cronJob(hour, minute, second, taskname)
 #     print("Hey!")
 
@@ -60,9 +90,3 @@ while True:
 #             pass
 #     except (KeyboardInterrupt, SystemExit):
 #         scheduler.shutdown()
-
-
-# def cronJob(hr, min, sec, taskname):
-#     scheduler.add_job(
-#         notifier.notifier, "cron", hour=hr, minute=min, second=sec, args=[taskname]
-#     )
