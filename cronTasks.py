7e2b6b251c3dd5b43023ef8992e088e78bba5b23
Santo <santosh@transak.com>
2023-11-01 11:16:36 +0530

    Working with the data as Dictionary

--- cronTasks.py
+++ cronTasks.py
@@ -1,59 +1,68 @@
 import sqlite3
 from apscheduler.schedulers.background import BackgroundScheduler
-from notifyy import Notify
 from datetime import datetime
+import notifier
+import time
 
 conn = sqlite3.connect("app.db")
 cursor = conn.cursor()
 
-
-def notifier(taskname):
-    notification = Notify()
-    notification.title = "Task Scheduler"
-    notification.message = taskname
-    notification.application_name = "jeevu"
-    notification.icon = "logo.png"
-    notification.send()
-
-
-def getTaskid():
-    task_ids = []
-    t = datetime.now()
-    curr_time = t.strftime("%H:%M:%S")
-    cursor.execute("SELECT * FROM tasks where duedate > ?", (curr_time,))
-    tasks = cursor.fetchall()
-    for ids in tasks:
-        task_ids.append(ids[0])
-    return task_ids
-
-
 scheduler = BackgroundScheduler()
 
 
-def cronJob(hr, min, sec, taskname):
-    scheduler.add_job(notifier, "cron", hour=hr, minute=min,
-                      second=sec, args=[taskname])
+# @TO_DO: Take userId as a parameter
+def getDueTasks():
+    allTasks = []
+    t = datetime.now()
+    curr_time = t.strftime("%H:%M:%S")
+    cursor.execute("SELECT * FROM tasks")
+    #    where duedate > ?", (curr_time,))
+    tasks = cursor.fetchall()
+    for ids in tasks:
+        __task = {
+            "user_id": ids[0],
+            "task_id": ids[1],
+            "taskname": ids[2],
+            "duedate": ids[3],
+        }
+        allTasks.append(__task)
+    print(allTasks)
+    return allTasks
 
 
-ids = getTaskid()
-for id in ids:
-    print(id)
-    cursor.execute("SELECT * FROM tasks where task_id = ?", (str(id),))
-    trigger_task = cursor.fetchone()
-    duedate = trigger_task[3]
-    time_obj = datetime.strptime(duedate, "%H:%M:%S")
-    hour = time_obj.hour
-    minute = time_obj.minute
-    second = time_obj.second
-    taskname = trigger_task[2]
-    print(taskname)
-    cronJob(hour, minute, second, taskname)
-    print("Hey!")
-
-if __name__ == "__main__":
+def cronos():
+    scheduler.add_job(getDueTasks, "interval", seconds=3)
     scheduler.start()
-    try:
-        while True:
-            pass
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
+
+
+while True:
+    cronos()
+    time.sleep(1)
+
+
+# ids = getDueTasks()
+
+# for id in ids:
+#     duedate = id["duedate"]
+#     taskname = id["taskname"]
+#     time_obj = datetime.strptime(duedate, "%H:%M:%S")
+#     hour = time_obj.hour
+#     minute = time_obj.minute
+#     second = time_obj.second
+#     print(taskname)
+#     cronJob(hour, minute, second, taskname)
+#     print("Hey!")
+
+# if __name__ == "__main__":
+#     scheduler.start()
+#     try:
+#         while True:
+#             pass
+#     except (KeyboardInterrupt, SystemExit):
+#         scheduler.shutdown()
+
+
+# def cronJob(hr, min, sec, taskname):
+#     scheduler.add_job(
+#         notifier.notifier, "cron", hour=hr, minute=min, second=sec, args=[taskname]
+#     )
